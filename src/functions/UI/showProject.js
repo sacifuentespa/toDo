5bebd4d5e0a0ab62997fb2a4fa953e4c66a99a02
Santiago <sacifuentespawk@gmail.com>
2024-01-16 19:06:13 -0500

    added first tasks views to projects

--- src/functions/UI/showProject.js
+++ src/functions/UI/showProject.js
@@ -1,5 +1,6 @@
 import {loadDataFromLocalStorage} from '../functionalities/projectController.js';
-
+import taskCard from './showTaskCard.js';
+import showTaskCard from './showTaskCard.js';
 
 const showProject = function(name){
     const projects = loadDataFromLocalStorage();
@@ -14,6 +15,14 @@ const showProject = function(name){
     const h2 = document.createElement('h2');
     h2.textContent = projectToShow.name
     projectContentDiv.appendChild(h2);
+
+    if(projectToShow.projectTasks.length > 0){
+        projectToShow.projectTasks.forEach((task) => {
+            const taskCard = showTaskCard(task);
+            projectContentDiv.appendChild(taskCard);
+        });
+        
+    };
 }
 
 export default showProject;
--- src/functions/UI/showTaskCard.js
+++ src/functions/UI/showTaskCard.js
@@ -1,19 +1,34 @@
 const taskCard = function(task){
     
     //waiting for implementation of specific logic for showing taskCards
-    /*
-    const projects = loadDataFromLocalStorage();
-    const mainContent = document.querySelector('.mainContent');
-    mainContent.innerHTML = '';
+    const divTaskCard = document.createElement('div');
+    divTaskCard.classList.add('divTaskCard');
 
-    const projectContentDiv = document.createElement('div');
-    projectContentDiv.classList.add('projectContentDiv');
-    mainContent.appendChild(projectContentDiv);
+    const titleTaskCard = document.createElement('h3');
+    titleTaskCard.textContent = task.title;
+    divTaskCard.appendChild(titleTaskCard);
     
-    const projectToShow = projects.find(project => project.name === name);
-    const h2 = document.createElement('h2');
-    h2.textContent = projectToShow.name
-    projectContentDiv.appendChild(h2);*/
+    const descriptionTaskCard = document.createElement('p');
+    descriptionTaskCard.textContent = task.description;
+    divTaskCard.appendChild(descriptionTaskCard);
+
+    const priorityTaskCard = document.createElement('p');
+    priorityTaskCard.textContent = task.priority;
+    divTaskCard.appendChild(priorityTaskCard);
+
+    const dateTaskCard = document.createElement('p');
+    dateTaskCard.textContent = task.date;
+    divTaskCard.appendChild(dateTaskCard);
+
+    const buttonEditTaskCard = document.createElement('button');
+    buttonEditTaskCard.textContent = "Edit task";
+    divTaskCard.appendChild(buttonEditTaskCard);
+
+    const buttonDeleteTaskCard = document.createElement('button');
+    buttonDeleteTaskCard.textContent = "Delete task";
+    divTaskCard.appendChild(buttonDeleteTaskCard);
+
+    return divTaskCard;
 }
 
 export default taskCard;
